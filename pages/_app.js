1d82e43a5210a84b86eac29ea50a3d0a66c1df60
CygnusX-26 <qh4nveh@gmail.com>
2023-07-07 01:16:43 -0700

    add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 
 import '../styles/globals.css'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function App({ Component, pageProps }) {
-    return <ThemeProvider enableSystem={false}><Component {...pageProps} /></ThemeProvider>
+    return (
+        <ThemeProvider enableSystem={false}>
+            <Component {...pageProps} />
+            <Analytics />
+        </ThemeProvider>
+    )
 }
